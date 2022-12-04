8c7278a6eb24a286a760ea99f4b7c71efe48b7bf
7777kdhangar <118211876+7777kdhangar@users.noreply.github.com>
2022-12-04 11:35:39 +0530

    Add files via upload

---
+++ apicall.py
@@ -0,0 +1,11 @@
+def apicall():
+    print("api finction from api module")
+def m1():
+    print("m1 in apicall")
+def m2():
+    print("m2 in apicall")
+class test:
+    def __init__(self):
+        print("init method of test calss")
+    def m5(self):
+        print("m5 in test class")
